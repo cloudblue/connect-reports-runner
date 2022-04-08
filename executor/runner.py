110fa691c3276139abe99a883f20e224d8ca1f42
Francesco Faraone <francesco.faraone@cloudblue.com>
2022-04-08 10:32:32 +0200

    Log stdout stderr if exit != 0

--- executor/runner.py
+++ executor/runner.py
@@ -1,6 +1,5 @@
 import logging
 import subprocess
-import time
 
 from connect.client import ClientError, ConnectClient
 
@@ -26,40 +25,37 @@ def run_executor():
             '-m',
             'executor.executor',
         ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
     )
     logger.info(f'Executor started: PID {proc.pid}, report: {report_id}')
-    while True:
-        returncode = proc.poll()
+    stdout, stderr = proc.communicate()
 
-        if returncode is None:
-            time.sleep(3)
-            continue
-
-        if returncode == 0:
-            logger.info('Executor process has exited with 0.')
-            return
-
-        logger.error(f'Executor process has exited with {returncode}.')
-
-        client = ConnectClient(
-            endpoint=report_env['api_endpoint'],
-            use_specs=False,
-            api_key=report_env['client_token'],
-            max_retries=3,
-            default_headers=get_user_agent(),
-        )
-        try:
-            fail_report(
-                client,
-                report_id,
-                'Report execution has failed due contains too much data, please try to exclude '
-                'using report parameters some of it and try again.',
-                False,
-            )
-            logger.info(f'Report {report_id} has been failed successfully.')
-        except ClientError as ce:
-            logger.warning(f'Cannot switch report {report_id} to fail status: {ce}')
+    if proc.returncode == 0:
+        logger.info('Executor process has exited with 0.')
         return
+
+    logger.error(f'Executor process has exited with {proc.returncode}: {stdout=} {stderr=}.')
+
+    client = ConnectClient(
+        endpoint=report_env['api_endpoint'],
+        use_specs=False,
+        api_key=report_env['client_token'],
+        max_retries=3,
+        default_headers=get_user_agent(),
+    )
+    try:
+        fail_report(
+            client,
+            report_id,
+            'Report execution has failed due contains too much data, please try to exclude '
+            'using report parameters some of it and try again.',
+            False,
+        )
+        logger.info(f'Report {report_id} has been failed successfully.')
+    except ClientError as ce:
+        logger.warning(f'Cannot switch report {report_id} to fail status: {ce}')
+    return
 
 
 if __name__ == '__main__':  # pragma: no cover
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -6,12 +6,15 @@ from executor.runner import run_executor
 
 
 def test_runner_exit_ok(mocker, mocked_env, caplog):
-    process_poll = mocker.MagicMock(
-        side_effect=[None, 0],
+    communicate_mock = mocker.MagicMock(
+        return_value=('stdout', 'stderr'),
     )
     mocker.patch(
         'executor.runner.subprocess.Popen',
-        return_value=mocker.MagicMock(poll=process_poll),
+        return_value=mocker.MagicMock(
+            communicate=communicate_mock,
+            returncode=0,
+        ),
     )
 
     with caplog.at_level(logging.INFO):
@@ -21,12 +24,15 @@ def test_runner_exit_ok(mocker, mocked_env, caplog):
 
 
 def test_runner_exit_ko_fail_ok(mocker, mocked_env, caplog):
-    process_poll = mocker.MagicMock(
-        side_effect=[None, 127],
+    communicate_mock = mocker.MagicMock(
+        return_value=('stdout', 'stderr'),
     )
     mocker.patch(
         'executor.runner.subprocess.Popen',
-        return_value=mocker.MagicMock(poll=process_poll),
+        return_value=mocker.MagicMock(
+            communicate=communicate_mock,
+            returncode=127,
+        ),
     )
 
     fail_mock = mocker.patch('executor.runner.fail_report')
@@ -34,18 +40,23 @@ def test_runner_exit_ko_fail_ok(mocker, mocked_env, caplog):
     with caplog.at_level(logging.INFO):
         run_executor()
 
-    assert caplog.records[1].message == 'Executor process has exited with 127.'
+    assert caplog.records[1].message == (
+        "Executor process has exited with 127: stdout='stdout' stderr='stderr'."
+    )
     assert caplog.records[2].message.endswith('has been failed successfully.')
     fail_mock.assert_called_once()
 
 
 def test_runner_exit_ko_fail_fail(mocker, mocked_env, caplog):
-    process_poll = mocker.MagicMock(
-        side_effect=[None, 127],
+    communicate_mock = mocker.MagicMock(
+        return_value=('stdout', 'stderr'),
     )
     mocker.patch(
         'executor.runner.subprocess.Popen',
-        return_value=mocker.MagicMock(poll=process_poll),
+        return_value=mocker.MagicMock(
+            communicate=communicate_mock,
+            returncode=127,
+        ),
     )
 
     fail_mock = mocker.patch('executor.runner.fail_report', side_effect=ClientError('test error'))
@@ -53,6 +64,8 @@ def test_runner_exit_ko_fail_fail(mocker, mocked_env, caplog):
     with caplog.at_level(logging.INFO):
         run_executor()
 
-    assert caplog.records[1].message == 'Executor process has exited with 127.'
+    assert caplog.records[1].message == (
+        "Executor process has exited with 127: stdout='stdout' stderr='stderr'."
+    )
     assert caplog.records[2].message.endswith(' to fail status: test error')
     fail_mock.assert_called_once()
