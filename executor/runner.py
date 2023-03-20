a982f8ba571e1ba6bd1e3d7468078dc7c37f4c69
Aleksandra Ovchinnikova <nsanjka@gmail.com>
2023-03-20 15:06:45 +0100

    LITE-26841 Save report exception traceback

--- executor/runner.py
+++ executor/runner.py
@@ -51,6 +51,7 @@ def run_executor():
             'Report execution has failed due contains too much data, please try to exclude '
             'using report parameters some of it and try again.',
             False,
+            stdout,
         )
         logger.info(f'Report {report_id} has been failed successfully.')
     except ClientError as ce:
